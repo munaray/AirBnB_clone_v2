1f58f51ea7b9d06e576041e008f5dac7be8ba55e
Ronnie-Leon76 <ronleon76@gmail.com>
2023-09-21 02:53:48 +0300

    feat: commented object lists implementation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        # if cls is not None:
+        #     for k, v in FileStorage.__objects.items():
+        #         if cls in k:
+        #             return {k: v}
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +52,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.all():
+                del self.all()[key]
+                self.save()
